a94082a9baf604ea8a57d48b8e9204457f1e4e66
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2025-09-06 13:48:52 -0700

    Add test framework

---
+++ tests/AppTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class AppTest extends TestCase
+{
+
+    public function testDummyAssertion(): void
+    {
+        $this->assertTrue(true, 'Dummy assertion');
+    }
+
+}
